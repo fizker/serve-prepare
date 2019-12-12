b4b8623dbf00318ed2d11d937d9c02c61d640b35
Benjamin Horsleben <benjamin@fizkerinc.dk>
2019-12-12 20:02:49 +0100

    Added some test dependencies

---
+++ .mocharc.js
@@ -0,0 +1,5 @@
+module.exports = {
+	recursive: true,
+	reporter: "dot",
+	ui: "bdd",
+}
---
+++ test/unit/getAllFolders.js
@@ -0,0 +1,10 @@
+// @flow strict
+
+const { describe, it, beforeEach } = require("mocha")
+const { expect } = require("chai")
+
+describe("unit/getAllFolders.js", () => {
+	describe("calling with empty array", () => {
+		it("should return empty array")
+	})
+})
