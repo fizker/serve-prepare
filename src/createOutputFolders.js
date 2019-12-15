56ebb1da7f788bbbf430e1b1d6bbe07a7533f54a
Benjamin Horsleben <benjamin@fizkerinc.dk>
2019-12-15 15:05:08 +0100

    Added function for ensuring that the output folders are setup up

---
+++ src/createOutputFolders.js
@@ -0,0 +1,30 @@
+// @flow strict
+
+const path = require("path")
+const fs = require("fs")
+
+/*::
+import type { Folders } from "./types"
+*/
+
+module.exports = createOutputFolders
+async function createOutputFolders(outputDir/*: string*/) /*: Promise<Folders> */ {
+	const folders = {
+		identity: path.join(outputDir, "files"),
+		deflate: path.join(outputDir, "deflate"),
+		gzip: path.join(outputDir, "gzip"),
+		brotli: path.join(outputDir, "brotli"),
+	}
+
+	await fs.promises.mkdir(outputDir,
+		// $FlowFixMe flow defs in 0.113.0 are broken
+		{ recursive: true })
+	await Promise.all([
+		fs.promises.mkdir(folders.identity),
+		fs.promises.mkdir(folders.deflate),
+		fs.promises.mkdir(folders.gzip),
+		fs.promises.mkdir(folders.brotli),
+	])
+
+	return folders
+}
