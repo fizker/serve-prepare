d527bfa0170a09236a6cf29ae2e1375b2666ad00
Benjamin Horsleben <benjamin@fizkerinc.dk>
2019-12-18 22:15:15 +0100

    Updated the main entry file to prepare for multiple commands

--- index.js
+++ index.js
@@ -1,79 +1,44 @@
 // @flow strict
 
-const fs = require("fs")
-const path = require("path")
-const util = require("util")
-const glob = util.promisify(require("glob"))
-const mime = require("mime")
+const buildCommand = require("./src/commands/build")
 
-const assertSetupRequest = require("./src/assertSetupRequest")
-const createOutputFolders = require("./src/createOutputFolders")
-const prepareOutputFiles = require("./src/prepareOutputFiles")
+const [ , , command, ...argv ] = process.argv
 
-/*::
-import type { ServerSetup, Folders } from "./src/types"
-*/
+const usageText =
+`Usage: serve-prepare <command> <command options ...>
 
-const [ , , requestPath ] = process.argv
-const absRequestPath = path.resolve(requestPath)
+Supported commands:
+	build <path to request config>
+`.trim()
 
-prepareFiles(absRequestPath)
+class UnknownCommandError extends Error {
+	constructor(command/*: string*/) {
+		super(
+`Unknown command: ${command}.
+
+${usageText}`
+		)
+	}
+}
+
+async function exec(command/*: ?string*/, argv/*: $ReadOnlyArray<string>*/) /*: Promise<void>*/ {
+	if(command == null) {
+		throw new Error(usageText)
+	}
+
+	switch(command) {
+		case "build":
+			return buildCommand(argv)
+		default:
+			throw new UnknownCommandError(command)
+	}
+}
+
+exec(command, argv)
 	.then(
 		() => { console.log("all done") },
 		(e) => {
-			console.error(e.stack)
+			console.error(e.message)
 			process.exit(1)
 		},
 	)
-
-async function prepareFiles(requestPath/*: string*/) /*: Promise<ServerSetup> */ {
-	const rawRequest = await fs.promises.readFile(requestPath, "utf-8")
-	const request = assertSetupRequest(JSON.parse(rawRequest))
-
-	const baseDir = path.dirname(requestPath)
-	const targetDir = path.resolve(baseDir, request.target)
-	const outputDir = path.resolve(baseDir, request.output)
-
-	await fs.promises.rmdir(outputDir,
-		// $FlowFixMe flow 0.113 does not support the recursive statement
-		{ recursive: true })
-
-	const allFiles = await glob("**/*", { cwd: targetDir, nodir: true })
-	const folders = await createOutputFolders(outputDir)
-	const sizes = await prepareOutputFiles(targetDir, folders, allFiles)
-
-	const setup = {
-		aliases: [],
-		folders: denormalizeFolders(outputDir, folders),
-		catchAllFile: null,
-		globalHeaders: request.globalHeaders,
-		files: sizes.map(({ path, sizes }) => {
-			const overrides = request.files.find(x => x.path === path) || { headers: {} }
-			const mimeType = overrides.mime || "" || mime.getType(path)
-			if(mimeType == null) {
-				throw new Error(`Could not determine mime-type for ${path}`)
-			}
-
-			return {
-				path,
-				sizes,
-				statusCode: overrides.statusCode || 0 || 200,
-				headers: overrides.headers,
-				mime: mimeType,
-			}
-		}),
-	}
-
-	await fs.promises.writeFile(path.join(outputDir, "setup.json"), JSON.stringify(setup, null, 2))
-
-	return setup
-}
-
-function denormalizeFolders(outputDir/*: string*/, absoluteFolders/*: Folders*/) /*: Folders */ {
-	return {
-		identity: path.relative(outputDir, absoluteFolders.identity),
-		deflate: path.relative(outputDir, absoluteFolders.deflate),
-		gzip: path.relative(outputDir, absoluteFolders.gzip),
-		brotli: path.relative(outputDir, absoluteFolders.brotli),
-	}
-}
---
+++ src/commands/build.js
@@ -0,0 +1,68 @@
+// @flow strict
+
+const fs = require("fs")
+const path = require("path")
+const util = require("util")
+const glob = util.promisify(require("glob"))
+const mime = require("mime")
+
+const assertSetupRequest = require("../assertSetupRequest")
+const createOutputFolders = require("../createOutputFolders")
+const prepareOutputFiles = require("../prepareOutputFiles")
+
+/*::
+import type { ServerSetup, Folders } from "../types"
+*/
+
+module.exports = cmd
+
+async function cmd([ rawRequestPath ]/*: $ReadOnlyArray<string>*/) {
+	const requestPath = path.resolve(rawRequestPath)
+	const rawRequest = await fs.promises.readFile(requestPath, "utf-8")
+	const request = assertSetupRequest(JSON.parse(rawRequest))
+
+	const baseDir = path.dirname(requestPath)
+	const targetDir = path.resolve(baseDir, request.target)
+	const outputDir = path.resolve(baseDir, request.output)
+
+	await fs.promises.rmdir(outputDir,
+		// $FlowFixMe flow 0.113 does not support the recursive statement
+		{ recursive: true })
+
+	const allFiles = await glob("**/*", { cwd: targetDir, nodir: true })
+	const folders = await createOutputFolders(outputDir)
+	const sizes = await prepareOutputFiles(targetDir, folders, allFiles)
+
+	const setup = {
+		aliases: [],
+		folders: denormalizeFolders(outputDir, folders),
+		catchAllFile: null,
+		globalHeaders: request.globalHeaders,
+		files: sizes.map(({ path, sizes }) => {
+			const overrides = request.files.find(x => x.path === path) || { headers: {} }
+			const mimeType = overrides.mime || "" || mime.getType(path)
+			if(mimeType == null) {
+				throw new Error(`Could not determine mime-type for ${path}`)
+			}
+
+			return {
+				path,
+				sizes,
+				statusCode: overrides.statusCode || 0 || 200,
+				headers: overrides.headers,
+				mime: mimeType,
+			}
+		}),
+	}
+
+	await fs.promises.writeFile(path.join(outputDir, "setup.json"), JSON.stringify(setup, null, 2))
+}
+
+function denormalizeFolders(outputDir/*: string*/, absoluteFolders/*: Folders*/) /*: Folders */ {
+	return {
+		identity: path.relative(outputDir, absoluteFolders.identity),
+		deflate: path.relative(outputDir, absoluteFolders.deflate),
+		gzip: path.relative(outputDir, absoluteFolders.gzip),
+		brotli: path.relative(outputDir, absoluteFolders.brotli),
+	}
+}
